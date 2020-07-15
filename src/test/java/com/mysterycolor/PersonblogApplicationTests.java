7de3823a7e60b8850b004846a805dad09026cab6
xujie <xujie@unionpay.com>
2020-07-15 09:37:57 +0800

    first commit

---
+++ src/test/java/com/mysterycolor/PersonblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysterycolor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
